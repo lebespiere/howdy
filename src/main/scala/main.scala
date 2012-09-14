d05683f80833879db5642b7033df63b5a3120211
Aleksey Voskobojnikov <lebespiere@ya.ru>
2012-09-14 11:50:53 +0400

    Added the /hello/:name route with corresponding greeting functionality.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,15 @@
 package sandbox
 
 import ru.circumflex._, core._, web._, freemarker._
-import java.util.Date
 
 class Main extends Router {
   val log = new Logger("sandbox")
 
-  'currentDate := new Date
-
-  get("/test") = "I'm fine, thanks!"
   get("/") = ftl("index.ftl")
+
+  get("/hello/:name") = {
+    'name := param("name")
+    ftl("name.ftl")
+  }
 
 }
---
+++ src/main/webapp/templates/name.ftl
@@ -0,0 +1,8 @@
+[#ftl]
+
+[#assign content]
+<h1>Hello ${name}!</h1>
+<p>You're awesome!</p>
+[/#assign]
+
+[#include "/layout.ftl"/]
